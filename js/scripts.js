9051b0e57a40d9a7655e35059f7cb6fb12059003
Nikitin Ruslan <mystudychannel.0@gmail.com>
2023-04-12 11:40:29 +0300

    created main sections switch mechanism

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function() {
     let selectCards = document.querySelectorAll('.information-select__card');
     let content = document.querySelectorAll('.information');
+    let headerBtns = document.querySelectorAll('.header__navigation-btn')
+    let mainSections = document.querySelectorAll('.main-section')
     
     selectCards.forEach(item => item.addEventListener('click', e => {
         let selectCardsTarget = e.target.getAttribute('data-card');
@@ -9,6 +11,26 @@ window.addEventListener('load', function() {
         item.classList.add('is-active');
         document.getElementById(selectCardsTarget).classList.add('is-active');
     }));
+
+    headerBtns.forEach(item => item.addEventListener('click', e => {
+
+        // Prevent default behaviour - reload
+        e.preventDefault()
+
+        let targetId = 'main-' + e.target.getAttribute('for')
+        
+        mainSections.forEach(item => {
+
+            item.classList.remove('is-active')
+
+            if (item.id == targetId) {
+                item.classList.add('is-active')
+            }
+        })
+
+        headerBtns.forEach(item => item.classList.remove('is-active'))
+        e.target.classList.add('is-active')
+    }))
 
     document.querySelectorAll('.information-rules__title').forEach((el) => {
         el.addEventListener('click', () => {
